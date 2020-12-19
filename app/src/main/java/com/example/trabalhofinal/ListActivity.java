daa00ffbbfa521ebbdbfa0d302f12e2bb8f9f1d5
maiconm <maiconm23@gmail.com>
2020-12-19 10:22:54 -0300

    Abre imagem no browser

--- app/src/main/java/com/example/trabalhofinal/ListActivity.java
+++ app/src/main/java/com/example/trabalhofinal/ListActivity.java
@@ -6,7 +6,10 @@ import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
@@ -23,8 +26,16 @@ public class ListActivity extends AppCompatActivity {
 
         listView = findViewById(R.id.list);
 
-        ArrayList<String> likesList = getIntent().getStringArrayListExtra("likesList");
+        final ArrayList<String> likesList = getIntent().getStringArrayListExtra("likesList");
         ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, likesList);
         listView.setAdapter(arrayAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(likesList.get(position)));
+                startActivity(browserIntent);
+            }
+        });
     }
 }
