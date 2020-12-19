d8ea260c1be2d54870c9ae3d915d2d18c4aa962a
maiconm <maiconm23@gmail.com>
2020-12-19 11:03:25 -0300

    Adiciona conteúdo na tela Sobre

--- app/src/main/java/com/example/trabalhofinal/AboutActivity.java
+++ app/src/main/java/com/example/trabalhofinal/AboutActivity.java
@@ -2,7 +2,10 @@ package com.example.trabalhofinal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 public class AboutActivity extends AppCompatActivity {
 
@@ -10,5 +13,26 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+    }
+
+    public void openAuthorRepo(View view) {
+        openUrl("https://github.com/maiconm");
+    }
+
+    public void openGithubRepo(View view) {
+        openUrl("https://github.com/maiconm/trabalho-final-android-utfpr");
+    }
+
+    public void openDogApi(View view) {
+        openUrl("https://dog.ceo/dog-api/");
+    }
+
+    public void openInstagramLikeAnimationRepo(View view) {
+        openUrl("https://github.com/stevdza-san/Instagram-Like-Animation-Android-Studio");
+    }
+
+    public void openUrl(String url) {
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+        startActivity(browserIntent);
     }
 }
